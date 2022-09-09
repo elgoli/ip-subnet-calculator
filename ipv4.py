ead1f24c69f1d38c390b2f4512145cf528dc92c5
elgol <>
2022-09-09 22:22:53 +0300

    Add support for reading subnetmask

--- ipv4.py
+++ ipv4.py
@@ -11,6 +11,18 @@ def is_valid_ip_address(ip_address):
            and 0 <= int(ip_address_octets[3]) <= 255
 
 
+def is_valid_subnet_mask(subnet_mask):
+    subnet_mask_octets = subnet_mask.split('.')
+    valid_subnet_masks = [255, 254, 252, 248, 240, 224, 192, 128, 0]
+    return (len(subnet_mask_octets) == 4) \
+           and (int(subnet_mask_octets[0]) in valid_subnet_masks) \
+           and (int(subnet_mask_octets[1]) in valid_subnet_masks) \
+           and (int(subnet_mask_octets[2]) in valid_subnet_masks) \
+           and (int(subnet_mask_octets[3]) in valid_subnet_masks) \
+           and (int(subnet_mask_octets[0]) >= int(subnet_mask_octets[1]) >= int(subnet_mask_octets[2]) >= int(
+        subnet_mask_octets[3]))
+
+
 if __name__ == "__main__":
     try:
         while True:
@@ -20,6 +32,14 @@ if __name__ == "__main__":
             else:
                 print("\nThe IP address is INVALID, enter a valid IPv4 address!")
                 continue
+        while True:
+            subnet_mask = input("\nEnter a subnetmask: ")
+            if is_valid_subnet_mask(subnet_mask):
+                break
+            else:
+                print("\nThe subnetmask is INVALID, enter a valid subnetmask!")
+                continue
+
     except KeyboardInterrupt:
         print("/n Interrupted /n")
         sys.exit()
--- test_ipv4.py
+++ test_ipv4.py
@@ -8,3 +8,7 @@ class IpCalculator(unittest.TestCase):
     def test_is_valid_ip_address(self):
         self.assertFalse(ipv4.is_valid_ip_address("0.0.0.1"))
         self.assertTrue(ipv4.is_valid_ip_address("172.16.0.1"))
+
+    def test_is_valid_subnet_mask(self):
+        self.assertFalse(ipv4.is_valid_subnet_mask("1.0.0.0"))
+        self.assertTrue(ipv4.is_valid_subnet_mask("255.0.0.0"))
